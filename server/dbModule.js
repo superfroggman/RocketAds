d4d8b2d8dcdb95c69b3973f1b5973b2eaac84de7
superfroggman <49985901+superfroggman@users.noreply.github.com>
2020-12-19 23:32:31 +0100

    formatted stuff and started implementing coins

--- server/dbModule.js
+++ server/dbModule.js
@@ -2,41 +2,37 @@ const mongoose = require("mongoose"),
   fs = require("fs");
 let db;
 
-//Connect to MongoDB With Authentication. 
+//Connect to MongoDB With Authentication.
 exports.cnctDBAuth = (collectionname) => {
-    const mongAuth = require('./mongoauth.json')
-    mongoose.connect(
-        "mongodb://localhost:27017/" + collectionname,
-        {
-            "auth": {
-                "authSource": "admin"
-            },
-            "user": mongAuth.username,
-            "pass": mongAuth.pass,
-            useNewUrlParser: true, 
-            useUnifiedTopology: true
-        }
-    );
+  const mongAuth = require("./mongoauth.json");
+  mongoose.connect("mongodb://localhost:27017/" + collectionname, {
+    auth: {
+      authSource: "admin",
+    },
+    user: mongAuth.username,
+    pass: mongAuth.pass,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 
-    db = mongoose.connection;
-    db.on('error', console.error.bind(console, 'connection error:'));
-    db.once('open', function () {
-        console.log("Connected to MongoDB using collection " + collectionname)
-    });
-}
+  db = mongoose.connection;
+  db.on("error", console.error.bind(console, "connection error:"));
+  db.once("open", function () {
+    console.log("Connected to MongoDB using collection " + collectionname);
+  });
+};
 
 //Connect to MongoDB
 exports.cnctDB = (collectionname) => {
-    let dbLink = `mongodb://localhost/${collectionname}`
-    mongoose.connect(dbLink, { useNewUrlParser: true, useUnifiedTopology: true });
+  let dbLink = `mongodb://localhost/${collectionname}`;
+  mongoose.connect(dbLink, { useNewUrlParser: true, useUnifiedTopology: true });
 
-    db = mongoose.connection;
-    db.on('error', console.error.bind(console, 'connection error:'));
-    db.once('open', function () {
-        console.log("Connected to MongoDB using " + collectionname)
-    });
-
-}
+  db = mongoose.connection;
+  db.on("error", console.error.bind(console, "connection error:"));
+  db.once("open", function () {
+    console.log("Connected to MongoDB using " + collectionname);
+  });
+};
 
 //Finds "toFind" in Database on the Model provided
 exports.findInDBOne = async (Model, toFind) => {
@@ -48,8 +44,8 @@ exports.findInDB = async (Model, limit) => {
 };
 
 exports.findUserWithID = async (Model, toFind) => {
-  return await Model.findOne({ _id: toFind })
-}
+  return await Model.findOne({ _id: toFind });
+};
 
 //takes input with type Model. Saves that model in Database. Cant be used before cnctDB or cnctDBAuth.
 exports.saveToDB = (input) => {
--- server/models/User.js
+++ server/models/User.js
@@ -1,19 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 //Creates the UserSchema and exports it
 const UserSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    date: {
-        type: Date,
-        default: Date.now
-    }
+  name: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  coins: {
+    type: Number,
+    default: 0,
+  },
 });
 
 const User = mongoose.model("User", UserSchema);
